605ed251974d46f0341349d4d449c25e62643dea
jeffrey lopez <jeffreysofly0320@gmail.com>
2022-03-29 18:08:51 -0400

    Added structs and a class

--- main.cpp
+++ main.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
-
 using namespace std;
+
+class Enrollment{
+    public:
+    Enrollment(){
+        Student student;
+    }
+};
+
+struct Student{
+
+};
+
+struct Instructor{
+
+};
+
+struct Term{
+
+};
+
+struct Course{
+
+};
 
 int main(){
     cout << "Hello World!" << endl; 
+    Enrollment enroll;
 }
