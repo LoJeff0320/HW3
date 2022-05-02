e8af57d3ab88b250a7d2b87967f64d55d747d900
jeffrey lopez <jeffreysofly0320@gmail.com>
2022-05-01 20:30:57 -0400

    Adding objects

--- src/main.cpp
+++ src/main.cpp
@@ -20,6 +20,10 @@ struct Student{
 	string ID;
 	Course course;
 	string grade;
+
+	Student(){
+		
+	}
 };
 
 struct Instructor{
@@ -29,27 +33,38 @@ struct Instructor{
 };
 
 class Enrollment{
+	vector<Student> studentVec;
+
 	public:
 	Enrollment(){
-		Student student;
+		studentVec {};
+	}
+
+	void enroll(Student student){
+		studentVec.push_back(student);
 	}
 };
 
 int main(){
-    Enrollment enroll[6];
+    Enrollment enroll;
+	//reads the file
     fstream myfile;
 	myfile.open("C:\\Users\\jeffrey\\Desktop\\homework\\HW3\\data\\data.csv", ios::in);
 
+	//vector of strings used for string tokenizing
 	vector<string> tokens;
-
+	
 	if(myfile.is_open()){
 		string line;
 		
     	while (getline (myfile, line, ',')){
     		tokens.push_back(line);
     	}
-	}
+	}else cout << "File cant be found." << endl;
 
+	Student student{tokens[0]};
+
+	//iterating through tokens vector
 	for(int i = 0; i < tokens.size(); i++) {
       cout << tokens[i] << endl;
    	}
